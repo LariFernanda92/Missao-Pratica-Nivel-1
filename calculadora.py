379e022dca35d636df41caf4be83e5d5e0449190
LariFernanda92 <148596995+LariFernanda92@users.noreply.github.com>
2024-07-26 06:33:15 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,14 @@
+a = int(input("Digite o primeiro valor:"))
+b = int(input("Digite o segundo valor:"))
+
+resultado_soma = a + b
+print ("Resultado da soma:", resultado_soma)
+
+resultado_diminuição = a - b
+print ("Resultado da diminuição:" , resultado_diminuição)
+
+resultado_multiplicação = a * b
+print ("Resultado da multiplicação:" , resultado_multiplicação)
+
+resultado_divisão = a / b
+print ("Resultado da divisão:" , resultado_divisão)
